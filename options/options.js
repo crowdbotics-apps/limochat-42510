e9cff6bae957b83b74f6c5a3c05b5718a70f2f4d
Crowdbotics <team@crowdbotics.com>
2023-07-29 11:51:22 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "limochat-42510",
+  url: "https://limochat-42510.botics.co",
+  api: "https://limochat-42510.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
